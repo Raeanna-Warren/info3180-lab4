04b495408d4997b4f503909a70f5f5bc01e14a97
Raeanna-Warren <166166794+Raeanna-Warren@users.noreply.github.com>
2025-03-16 16:08:51 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 # import flask migrate here
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
